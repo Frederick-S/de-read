92faae2cea5b8c387ff48168d2c083fcfb7e9d7c
Frederick-S <mao_xiaodan@hotmail.com>
2014-10-31 07:26:49 +0000

    Init project

---
+++ test.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+var read = require('./lib/de-read.js');
+
+describe('Read time', function () {
+    
+});
+
+describe('Read year', function () {
+    
+});
+
+describe('Read number', function () {
+    
+});
